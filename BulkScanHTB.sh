3b388195baa7b2ae3788fcece97606e5e2a6ad02
Daniel(a) <ping.from.dani@protonmail.com>
2020-04-19 03:27:21 +0200

    Add files via upload

--- BulkScanHTB.sh
+++ BulkScanHTB.sh
@@ -14,33 +14,33 @@ then
 		else
 			echo "Starting scanning the targets..."
 			#######################################
-			cd ~/Documents/HTB
-			for dir in $(cat $targets | cut -d" " -f2); do mkdir ~/Documents/HTB/$repository/$dir; done
-			cd ~/Documents/HTB
-			for dir in $(cat $targets | cut -d" " -f2)
+			cd ~/Documents
+			for dir in $(cut -d" " -f2 "$targets"); do mkdir ~/Documents/HTB/$repository/$dir; done
+			cd ~/Documents
+			for dir in $(cut -d" " -f2 "$targets")
 				do 
 						cd ~/Documents/HTB/$repository/$dir 
 						#scan all TCP ports, but do not establish a session, just check state
-						nmap -Pn -sS -p 0- --reason $dir -oA initial-tcp-scan
+						nmap -Pn -sS -p 0- --reason "$dir" -oA initial-tcp-scan
 						#scan top 1000 UCP ports, set timeout for speed
-						nmap -Pn -sU --reason --host-timeout 100ms $dir -oA initial-udp-scan
+						nmap -Pn -sU --reason --host-timeout 100ms "$dir" -oA initial-udp-scan
 						cat initial-tcp-scan.xml |grep -i 'open"' |grep -i "portid=" |cut -d '"' -f 4,5,6| grep -o '[0-9]*' |sort --unique |paste -s -d, > nmap_unique_tcp_ports.txt
 						cat initial-udp-scan.xml |grep -i 'open"' |grep -i "portid=" |cut -d '"' -f 4,5,6| grep -o '[0-9]*' |sort --unique |paste -s -d, > nmap_unique_udp_ports.txt
 				done
-			cd ~/Documents/HTB
+			cd ~/Documents
 			for dir in $(cat $targets | cut -d" " -f2)
 				do 
 					cd ~/Documents/HTB/$repository/$dir
 					if [ $(wc -c <nmap_unique_tcp_ports.txt) -gt 1 ] 
 					then 
 						#echo "Test point"
-						nmap -sV -sC --reason -p $(cat nmap_unique_tcp_ports.txt) $dir -oA tcp-version-scan
-						nmap --script vuln -p $(cat nmap_unique_tcp_ports.txt) $dir -oA tcp-vuln-scan
+						nmap -sV -sC --reason -p $(cat nmap_unique_tcp_ports.txt) "$dir" -oA tcp-version-scan
+						nmap --script vuln -p $(cat nmap_unique_tcp_ports.txt) "$dir" -oA tcp-vuln-scan
 						#nmap --script default -p $(cat nmap_unique_tcp_ports.txt) $dir -oN tcp-script-scan.nmap 
 					fi
 					if [ $(wc -c <nmap_unique_udp_ports.txt) -gt 1 ] 
 					then 
-						nmap -sUV -A --reason -p $(cat nmap_unique_udp_ports.txt) $dir -oA udp-version-scan
+						nmap -sUV -A --reason -p $(cat nmap_unique_udp_ports.txt) "$dir" -oA udp-version-scan
 					fi
 				done
 		fi
@@ -50,4 +50,4 @@ then
 else 
 	echo "This directory is already populated with scans..."
 fi
-cd ~/Documents/HTB
+cd ~/Documents || exit
